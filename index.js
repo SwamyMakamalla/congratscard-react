5d2fc8f2ddc0a409c05fcc2d51733a0d9f929129
swamy <swamymakamalla@gmail.com>
2022-08-01 09:36:03 +0000

    congrats card using react

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="congratsContainer">
+    <h1 className="heading">Congratulations</h1>
+
+    <div className="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="nameHeading">Kiran v</h1>
+      <p className="text">
+        Vishnu institute of computer Education And Technology,Bhimavaram.
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
